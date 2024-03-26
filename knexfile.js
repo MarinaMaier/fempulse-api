e138865419ac35313959f6848c8d543976d84be1
MarinaMaier <157530878+MarinaMaier@users.noreply.github.com>
2024-03-26 16:31:47 -0400

    edit env file

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,10 @@ require("dotenv").config();
 module.exports = {
   client: "mysql",
   connection: {
-    host: process.env.DB_LOCAL_HOST,
-    database: process.env.DB_LOCAL_DBNAME,
-    user: process.env.DB_LOCAL_USER,
-    password: process.env.DB_LOCAL_PASSWORD,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     charset: "utf8mb4",
     collation: "utf8mb4_unicode_ci", // Specify collation for proper sorting and comparison
     // Add the following line to ensure UTF-8 encoding
